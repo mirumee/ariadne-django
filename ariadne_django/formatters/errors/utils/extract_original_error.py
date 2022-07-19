792a812b974b7a98f6e60ca4cdbfb9fa7d9c6e3a
Max Morlocke <kedlav@gmail.com>
2022-07-18 23:11:09 -0400

    Update ariadne_django/formatters/errors/utils/extract_original_error.py
    Co-authored-by: Drew Synan <dsynan@gmail.com>

--- ariadne_django/formatters/errors/utils/extract_original_error.py
+++ ariadne_django/formatters/errors/utils/extract_original_error.py
@@ -1,4 +1,4 @@
 def extract_original_error(error):
-    if getattr(error, "original_error", None):
+    if getattr(error, "original_error", None) is not None:
         return extract_original_error(error.original_error)
     return error
